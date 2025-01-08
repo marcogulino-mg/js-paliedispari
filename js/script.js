bbffd1caa897172a534249cb72850876a8cff9cc
marco-gulino <marcogulino98@gmail.com>
2025-01-08 16:57:05 +0100

    [feat] secondo exercise complete

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//PRIMO ESERCIZIO 
+//PRIMO ESERCIZIO
 /*
 PALINDROMA
 CHIEDERE ALL'UTENTE DI INSERIRE UNA PAROLA.
@@ -11,20 +11,88 @@ let word = prompt("Digita una parola!");
 //FUNCTIONs
 //Funzione per capire se la parola è palindroma
 function palindromeWord(inWord) {
-    //Stringa che conterrà la parola scritta al contrario
-    let reverseString = "";
-    //Concatenazione dei tre metodi per rovesciare la stringa
-    const tempWord = inWord.split("").reverse().join("");
-    console.log(tempWord);
+  //Stringa che conterrà la parola scritta al contrario
+  let reverseString = "";
+  //Concatenazione dei tre metodi per rovesciare la stringa
+  const tempWord = inWord.split("").reverse().join("");
+  console.log(tempWord);
 
-    //Controllo se la parola è palindroma
-    if(inWord === tempWord) {
-        return console.log("La parola è palindroma");
-    }
-    else {
-        return console.log("La parola non è palindroma");
-    }
+  //Controllo se la parola è palindroma
+  if (inWord === tempWord) {
+    return console.log("La parola è palindroma");
+  } else {
+    return console.log("La parola non è palindroma");
+  }
 }
 
 //Richiamo la funzione
 palindromeWord(word);
+
+//SECONDO ESERCIZIO
+/*
+PARI E DISPARI
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+
+//VARs
+let userChoice = prompt("Scegli pari o dispari");
+let number = parseInt(prompt("Inserisci un numero da 1 a 5"));
+let cpuNum = 0;
+let result;
+
+//FUNCTIONs
+//Funzione che genera un numero random da 1 a 5
+function numberGenerator() {
+  let randNum = Math.floor(Math.random() * 5) + 1;
+  return randNum;
+}
+
+//Funzione che controlla se il numero è pari o dispari
+function checkNum(choice, num) {
+  let isEven = false;
+  if (num % 2 === 0) {
+    console.log("Il numero è pari");
+    isEven = true;
+    return isEven;
+  }
+
+  console.log("Il numero è dispari");
+  return isEven;
+}
+
+console.log("Il numero che hai scelto è: " + number);
+
+//Creo un numero random da 1 a 5
+cpuNum = numberGenerator();
+console.log("Il numero della CPU è: " + cpuNum);
+
+//Sommo il numero random con il numero inserito dall'utente
+sum = number + cpuNum;
+console.log("La somma è uguale a : " + sum);
+
+//Richiamo la funzione per controllare se il numero è pari o dispari e dichiaro il vincitore
+result = checkNum(userChoice, sum);
+
+if (
+  (result && userChoice === "pari") ||
+  (result && userChoice === "Pari") ||
+  (!result && userChoice === "dispari") ||
+  (result && userChoice === "Dispari")
+) {
+  console.log("Il giocatore ha vinto");
+} else if (
+  userChoice != "pari" &&
+  userChoice != "Pari" &&
+  userChoice != "dispari" &&
+  userChoice != "Dispari"
+) {
+  console.log("Errore nella scelta tra pari o dispari");
+} else if (number > 5 || number < 1) {
+  console.log("Errore nell'inserimento del numero");
+} else {
+  console.log("La CPU ha vinto");
+}
